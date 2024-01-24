1fa42301ad9c432c44cb31e09f0f034878943ab7
Linh Pham My <phammylinh2002@gmail.com>
2024-01-25 02:27:06 +0700

    Refactored step 4 in the scrape_your_course function

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 from dotenv import load_dotenv
 import os
@@ -29,37 +27,47 @@ def scrape_your_course_data(username, password):
 
         # Step 3: Submit the login form
         password_input.send_keys(Keys.RETURN)
-        time.sleep(3)
+
 
         # Step 4: Get all courses removed from view and display each as a card
-        wait = WebDriverWait(driver, 10)
-        # Show removed-from-view courses
-        driver.find_element(By.XPATH, '//*[@id="groupingdropdown"]').click()
-        remove_from_view_xpath = '//a[contains(text(), "Removed from view")]'
-        wait.until(EC.element_to_be_clickable((By.XPATH, remove_from_view_xpath)))
-        driver.find_element(By.XPATH, remove_from_view_xpath).click()
-        time.sleep(1)
-        print("Displaying removed-from-view courses")
-        # Show all courses in a page 
-        driver.find_element(By.XPATH, '//button[contains(@aria-label, " items per page")]').click()
-        show_all_xpath = '//li[@data-limit="0"]'
-        wait.until(EC.element_to_be_clickable((By.XPATH, show_all_xpath)))
-        driver.find_element(By.XPATH, show_all_xpath).click()
-        time.sleep(1)
-        print("Displaying all courses in a page")
-        # Show each course as a card
-        driver.find_element(By.XPATH, '//*[@id="displaydropdown"]').click()
-        card_display_xpath = '//a[@data-value="card"]'
-        wait.until(EC.element_to_be_clickable((By.XPATH, card_display_xpath)))
-        driver.find_element(By.XPATH, card_display_xpath).click()
-        time.sleep(1)
-        print("Displaying each course as a card")
+        time.sleep(5)
+        display_options = [
+            {
+                'removed_from_view': {
+                    'dropdown_button':'//*[@id="groupingdropdown"]',
+                    'option':'//a[contains(text(), "Removed from view")]'
+                }
+            },
+            {
+                'show_all': {
+                    'dropdown_button':'//button[contains(@aria-label, " items per page")]',
+                    'option':'//li[@data-limit="0"]'
+                }
+            },
+            {
+                'card_display': {
+                    'dropdown_button':'//*[@id="displaydropdown"]',
+                    'option':'//a[@data-value="card"]'
+                }
+            }
+        ]        
+        for option in display_options:
+            for key, value in option.items():
+                dropdown_button_xpath = value['dropdown_button']
+                option_xpath = value['option']
+                print(dropdown_button_xpath)
+                print(option_xpath)
+            driver.find_element(By.XPATH, dropdown_button_xpath).click()
+            time.sleep(1)
+            driver.find_element(By.XPATH, option_xpath).click()
+            print(f"Display option '{key}' was chosen")
+            time.sleep(3)
 
         # Step 5: Scrape the content using BeautifulSoup
-        page_source = driver.page_source
-        soup = BeautifulSoup(page_source, 'html.parser')
-        with open('page_source.html', 'w', encoding='utf-8') as f:
-            f.write(page_source)
+        # page_source = driver.page_source
+        # soup = BeautifulSoup(page_source, 'html.parser')
+        # with open('page_source.html', 'w', encoding='utf-8') as f:
+        #     f.write(page_source)
         # Now you can use BeautifulSoup to extract the data you need
         # For example:
         # data = soup.find('div', class_='your-target-class').text
