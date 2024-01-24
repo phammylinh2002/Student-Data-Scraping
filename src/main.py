f6a773b8c0d2a493b9f9c9644734fb01e2b36171
Linh Pham My <phammylinh2002@gmail.com>
2024-01-24 19:46:47 +0700

    Complete step 4 - Get all courses removed from view and display each as a card

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from dotenv import load_dotenv
 import os
@@ -21,24 +22,38 @@ def scrape_your_course_data(username, password):
         driver.get(url)
         
         # Step 2: Find and fill in the login form
-        username_input = driver.find_element('name', 'username')
-        password_input = driver.find_element('name', 'password')
+        username_input = driver.find_element(By.NAME, 'username')
+        password_input = driver.find_element(By.NAME, 'password')
         username_input.send_keys(username)
         password_input.send_keys(password)
 
         # Step 3: Submit the login form
         password_input.send_keys(Keys.RETURN)
-
-        # Allow some time for the page to load
         time.sleep(3)
 
-        # Step 4: Get all courses removed from view
-        dropdown_button = driver.find_element('id', 'groupingdropdown')
-        dropdown_button.click()
+        # Step 4: Get all courses removed from view and display each as a card
+        wait = WebDriverWait(driver, 10)
+        # Show removed-from-view courses
+        driver.find_element(By.XPATH, '//*[@id="groupingdropdown"]').click()
+        remove_from_view_xpath = '//a[contains(text(), "Removed from view")]'
+        wait.until(EC.element_to_be_clickable((By.XPATH, remove_from_view_xpath)))
+        driver.find_element(By.XPATH, remove_from_view_xpath).click()
         time.sleep(1)
-        item = driver.find_element('path', '//a[@class="dropdown-item" and text()="Remove from view"]')
-        item.click()
+        print("Displaying removed-from-view courses")
+        # Show all courses in a page 
+        driver.find_element(By.XPATH, '//button[contains(@aria-label, " items per page")]').click()
+        show_all_xpath = '//li[@data-limit="0"]'
+        wait.until(EC.element_to_be_clickable((By.XPATH, show_all_xpath)))
+        driver.find_element(By.XPATH, show_all_xpath).click()
         time.sleep(1)
+        print("Displaying all courses in a page")
+        # Show each course as a card
+        driver.find_element(By.XPATH, '//*[@id="displaydropdown"]').click()
+        card_display_xpath = '//a[@data-value="card"]'
+        wait.until(EC.element_to_be_clickable((By.XPATH, card_display_xpath)))
+        driver.find_element(By.XPATH, card_display_xpath).click()
+        time.sleep(1)
+        print("Displaying each course as a card")
 
         # Step 5: Scrape the content using BeautifulSoup
         page_source = driver.page_source
