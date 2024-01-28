1b0945792f80cae0cb3d6a7b480cdcdd9d114fdf
Linh Pham My <phammylinh2002@gmail.com>
2024-01-28 13:45:54 +0700

    Remove unused imports

--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 from random import randint
 import time
